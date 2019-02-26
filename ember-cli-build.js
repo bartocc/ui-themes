57cfdef114a78b0c8295c15ff77bf88e044bf832
Julien Palmas <julien@crx.io>
2019-02-26 17:24:50 +0100

    build semantic for amaretti

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,7 +19,8 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('vendor/semantic/dist/semantic.css');
+
+  // app.import('vendor/semantic/dist/semantic.css');
 
   return app.toTree();
 };
